1954895a891123a60e252dd2629d7516f04d9f5d
Madhav <Madhavampire@gmail.com>
2025-01-27 00:45:51 +0530

    back to knowhere5.github.io

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/blogs/',
   server: {
     host: "::",
     port: 8080,
