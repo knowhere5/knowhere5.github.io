378700e8f1c664c0ab42a38c31c3e7249f2b51db
Madhav <Madhavampire@gmail.com>
2025-01-28 23:08:59 +0530

    href in logo

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,7 +1,7 @@
 const Bio = () => {
   return (
     <div className="mb-16 mt-16 pb-16 border-b">
-      <h1 className="text-3xl font-bolder mb-6 bol"><b>Madhav</b></h1>
+      <h1 className="text-3xl font-bolder mb-6 bol"><b><a href="https://madhav-mknc.me">Madhav</a></b></h1>
       <p className="text-gray-600 dark:text-gray-300 max-w-prose text-lg leading-relaxed">
         I majored in Engineering Physics at IIT Delhi, with a minor in Computer Science.
         I code, I produce music, and I think about transhumanism.
